dc633fd9a1a264c4be8c1b11be379c103d98ca51
dwang310 <davidwang@gatech.edu>
2016-12-07 22:32:39 -0500

    install script changed

--- init/install_script.R
+++ init/install_script.R
@@ -1,6 +1,6 @@
 # For Installation
 
-packageList <- list("readxl", "EpiModel", "ggplot2", "reshape2", "plotly", "shinythemes", "shinyBS", "dplyr", "shinyjs")
+packageList <- list("EpiModel", "ggplot2", "reshape2", "plotly", "shinythemes", "shinyBS", "dplyr", "shinyjs")
 
 for(item in packageList){
   install.packages(item)
